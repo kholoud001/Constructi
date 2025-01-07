eeefe92eb57f1f6c996ad4771ee7adc600568e07
kholoud001 <kholoud.sanak@gmail.com>
2025-01-07 17:26:07 +0100

    CON-6 role enums and entity

---
+++ src/main/java/com/constructi/model/entity/Role.java
@@ -0,0 +1,29 @@
+package com.constructi.model.entity;
+
+
+import com.constructi.model.enums.RoleType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+//    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+//    @JsonIgnore
+//    private List<User> users;
+}
---
+++ src/main/java/com/constructi/model/enums/RoleType.java
@@ -0,0 +1,8 @@
+package com.constructi.model.enums;
+
+public enum RoleType {
+
+    ROLE_ARCHITECT,
+    ROLE_ADMIN,
+    ROLE_WORKER
+}
