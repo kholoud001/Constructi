de116d65a4f9185ba8b142e3918b601110de3d8c
kholoud001 <kholoud.sanak@gmail.com>
2025-01-19 10:27:09 +0100

    CON-8 register

--- src/main/java/com/constructi/controller/AuthController.java
+++ src/main/java/com/constructi/controller/AuthController.java
@@ -3,8 +3,11 @@ package com.constructi.controller;
 import com.constructi.DTO.AuthenticationRequest;
 import com.constructi.DTO.AuthenticationResponse;
 import com.constructi.DTO.RegistrationRequest;
+import com.constructi.model.entity.Role;
 import com.constructi.model.entity.User;
 import com.constructi.model.enums.ContratType;
+import com.constructi.model.enums.RoleType;
+import com.constructi.repository.RoleRepository;
 import com.constructi.security.JwtUtils;
 import com.constructi.service.UserService;
 import com.constructi.service.impl.CustomUserDetailsService;
@@ -15,6 +18,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +33,10 @@ public class AuthController {
     private final JwtUtils jwtUtils;
     private final UserService userService;
     private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+
+
 
 
     @PostMapping("/register")
@@ -42,14 +50,23 @@ public class AuthController {
         user.setLname(registrationRequest.getLastName());
         user.setCell(registrationRequest.getCell());
         user.setEmail(registrationRequest.getEmail());
-        user.setPassword(registrationRequest.getPassword());
+
+        String hashedPassword = passwordEncoder.encode(registrationRequest.getPassword());
+        user.setPassword(hashedPassword);
+
         user.setRateHourly(registrationRequest.getRateHourly());
         user.setContratType(ContratType.valueOf(registrationRequest.getContratType()));
+
+        Role defaultRole = roleRepository.findByRoleType(RoleType.ROLE_WORKER)
+                .orElseThrow(() -> new RuntimeException("Default role not found"));
+        user.setRole(defaultRole);
 
         userService.save(user);
 
         return ResponseEntity.ok("User registered successfully");
     }
+
+
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody @Valid AuthenticationRequest authRequest) {
@@ -58,8 +75,12 @@ public class AuthController {
         );
 
         UserDetails userDetails = customUserDetailsService.loadUserByUsername(authRequest.getEmail());
-        String token = jwtUtils.generateToken(String.valueOf(userDetails));
+        System.out.println("Authenticated user: " + userDetails.getUsername());
+
+        String token = jwtUtils.generateToken(userDetails.getUsername());
+        System.out.println("Generated token: " + token);
 
         return ResponseEntity.ok(new AuthenticationResponse(token));
     }
+
 }
