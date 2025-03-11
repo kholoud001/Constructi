310de43edabc3a27e7e684f932f1fb267404dccb
kholoud001 <kholoud.sanak@gmail.com>
2025-03-11 15:10:38 +0000

    download styled invoice

--- constructi-frontend/src/app/modules/dashboard/architect-dashboard/architect-dashboard.component.ts
+++ constructi-frontend/src/app/modules/dashboard/architect-dashboard/architect-dashboard.component.ts
@@ -430,38 +430,38 @@ export class ArchitectDashboardComponent implements OnInit {
 
   async generatePdf(invoice: any): Promise<void> {
     // Function to convert an image URL to a base64 data URL
-    const fetchImageAsDataURL = async (url: string): Promise<string> => {
-      const response = await fetch(url);
-      const blob = await response.blob();
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result as string);
-        reader.onerror = reject;
-        reader.readAsDataURL(blob);
-      });
-    };
+    // const fetchImageAsDataURL = async (url: string): Promise<string> => {
+    //   const response = await fetch(url);
+    //   const blob = await response.blob();
+    //   return new Promise((resolve, reject) => {
+    //     const reader = new FileReader();
+    //     reader.onloadend = () => resolve(reader.result as string);
+    //     reader.onerror = reject;
+    //     reader.readAsDataURL(blob);
+    //   });
+    // };
 
     try {
       // Convert the logo URL to a base64 data URL
-      const logoUrl = 'https://media-hosting.imagekit.io//6a2add728e6e44d0/logo-black.png?Expires=1835622901&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=bBxCKQPMzfwgT8unST5rHsIifcRRIK3EnjtU8CS0kMNtN69IdALPdrWqRRdOLiDSHPLcxNSzNLZS4OPrNayTcmBUzB~5W0IkGsXK5L3E8wnAbhLhfnu3S2hukRJL~XpKoZUvX~L~fmob5fUw5jrmYsvH2~hhnqu2tvuiMfclnuQF2g8g2KQNFIBJPfMZTeOlyk06eRRRv00rbnB6LOwlLEIWkRQi2rFaEO1KSxb~sNVG62ica2mCsvMNozqBwjTWzortH1icmlhE9m72HwRabjO6CAexLd7-dgbjlofeFmG2SgDGmahh2F73Q7w4jfpGvT8EHcfwifbeXkfW2JCOlA__';
-      const logoDataURL = await fetchImageAsDataURL(logoUrl);
+      // const logoUrl = 'https://media-hosting.imagekit.io//6a2add728e6e44d0/logo-black.png?Expires=1835622901&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=bBxCKQPMzfwgT8unST5rHsIifcRRIK3EnjtU8CS0kMNtN69IdALPdrWqRRdOLiDSHPLcxNSzNLZS4OPrNayTcmBUzB~5W0IkGsXK5L3E8wnAbhLhfnu3S2hukRJL~XpKoZUvX~L~fmob5fUw5jrmYsvH2~hhnqu2tvuiMfclnuQF2g8g2KQNFIBJPfMZTeOlyk06eRRRv00rbnB6LOwlLEIWkRQi2rFaEO1KSxb~sNVG62ica2mCsvMNozqBwjTWzortH1icmlhE9m72HwRabjO6CAexLd7-dgbjlofeFmG2SgDGmahh2F73Q7w4jfpGvT8EHcfwifbeXkfW2JCOlA__';
+      // const logoDataURL = await fetchImageAsDataURL(logoUrl);
 
       // Define icons as data URLs (you may need to replace these with actual icon data URLs)
-      const icons = {
-        invoice: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAABCUlEQVR4nO2UQU7DMBBF3wVYcANOAeIQrOAMbOAKHIkjcJAKLsGSLhFbJCpxCaGq6gVgAWKDxtJYsT0Zx1nQv7Q0lv/z52fGCfwpgCmwBHbAJ6DAHngC5gM5Y2AFXAI3wAb4Ap6BKXBu688t+JuJXTsGHhK5GyJUgSKRXAEPwMI8RXAFvDrJH8C1E78DXpyNXoErgGsnuXLi3LwKYGZJe+AcmAEHk1sCJ8A9cLD1sxjAyAmqgBvg1vQN8G5yC+A0BeBZ9G7aKXDmtKlPMUvNi3rwDLw57VXzol70aT6oB2MBhgIkm/+/AUK/6D8A+OZjAZKDNhZAA0wGAQQXjQVoTK4ZzPkGCKcXxGOutecAAAAASUVORK5CYII=',
-        calendar: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAA2klEQVR4nO2UQQrCMBBFcwc9g3oE8SbqUhFX4lVE8CCC4ELduBc8guhSPIDoRqEmMGAkTZNJU3DhLNPkvzzm/0CVlBLQAGbABjgBV2APLIAu8BJ5DcwBPXAGbsAWGALVogKgBaQ5MQHqvxhXgWHO4SPQyTGuAOOChrP/GZt3n8/ZEL0Cc2VjAzjKmk+hCHgHekrGfeAuAStN4wh4ABMl477MRUArGiNgJGuekjHwlLmIjxHwkjVNxsCnFIAWGRcxzgOIL6UAtMjYFAFoFxkXMU4y/v8Bwiv6BYBvXgpAMi4FkGScFOADFzgXQIbf8AQAAAAASUVORK5CYII=',
-        user: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAABWUlEQVR4nO2UPUsDQRCGn0MEESxE/AEWdoKFjYVgYeGPsLESbPwLNhYiFlrZCYKFhYUgFhYiCoqIH6hRiGgMaMQvxGIHFuZgvdxe7mRBfGG53X1n3p2ZnYX/BqAGGAZ2gCxwB1wDu8AoUF2OfxMwADwBvgdPwDhQWYp5FbAI5Eu8vADMAbVhzHuAhxLGLrLA1JfmLcBNGcYuboEO37wDuI/AfAjzHPUh5vXARQTGLs4BBt+8GziMwXwfqPLMG4GzGMxdnAKNnnkbcB2juQWWPfM6IB2jeTqIqQrYitHcAhOeeTWwGqO5BZY887IvWQALnnkFsByDsQWWPPMY3+AXMOuZx/UNfgIznnmcSTYUMI/yJxsOmEeV5GQR8yiSvFfEvNwk7xUxLzXJe0XMwyZ5r4R52CTvlTD3k7wXwjzTBmyUYJwCWsMk+d8D+AA+gDfgGdgDhoDKb2v+W/EO8QdHzVvgE/UAAAAASUVORK5CYII=',
-        task: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAA5ElEQVR4nO2UQQrCMBBFcwc9g3oE8SbqUhFX4lVE8CCC4ELduBc8guhSPIDoRqEmMGAkTZNJU3DhLNPkvzzm/0CVlBLQAGbABjgBV2APLIAu8BJ5DcwBPXAGbsAWGALVogKgBaQ5MQHqvxhXgWHO4SPQyTGuAOOChrP/GZt3n8/ZEL0Cc2VjAzjKmk+hCHgHekrGfeAuAStN4wh4ABMl477MRUArGiNgJGuekjHwlLmIjxHwkjVNxsCnFIAWGRcxzgOIL6UAtMjYFAFoFxkXMU4y/v8Bwiv6BYBvXgpAMi4FkGScFOADFzgXQIbf8AQAAAAASUVORK5CYII='
-      };
+      // const icons = {
+      //   invoice: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAABCUlEQVR4nO2UQU7DMBBF3wVYcANOAeIQrOAMbOAKHIkjcJAKLsGSLhFbJCpxCaGq6gVgAWKDxtJYsT0Zx1nQv7Q0lv/z52fGCfwpgCmwBHbAJ6DAHngC5gM5Y2AFXAI3wAb4Ap6BKXBu688t+JuJXTsGHhK5GyJUgSKRXAEPwMI8RXAFvDrJH8C1E78DXpyNXoErgGsnuXLi3LwKYGZJe+AcmAEHk1sCJ8A9cLD1sxjAyAmqgBvg1vQN8G5yC+A0BeBZ9G7aKXDmtKlPMUvNi3rwDLw57VXzol70aT6oB2MBhgIkm/+/AUK/6D8A+OZjAZKDNhZAA0wGAQQXjQVoTK4ZzPkGCKcXxGOutecAAAAASUVORK5CYII=',
+      //   calendar: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAA2klEQVR4nO2UQQrCMBBFcwc9g3oE8SbqUhFX4lVE8CCC4ELduBc8guhSPIDoRqEmMGAkTZNJU3DhLNPkvzzm/0CVlBLQAGbABjgBV2APLIAu8BJ5DcwBPXAGbsAWGALVogKgBaQ5MQHqvxhXgWHO4SPQyTGuAOOChrP/GZt3n8/ZEL0Cc2VjAzjKmk+hCHgHekrGfeAuAStN4wh4ABMl477MRUArGiNgJGuekjHwlLmIjxHwkjVNxsCnFIAWGRcxzgOIL6UAtMjYFAFoFxkXMU4y/v8Bwiv6BYBvXgpAMi4FkGScFOADFzgXQIbf8AQAAAAASUVORK5CYII=',
+      //   user: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAABWUlEQVR4nO2UPUsDQRCGn0MEESxE/AEWdoKFjYVgYeGPsLESbPwLNhYiFlrZCYKFhYUgFhYiCoqIH6hRiGgMaMQvxGIHFuZgvdxe7mRBfGG53X1n3p2ZnYX/BqAGGAZ2gCxwB1wDu8AoUF2OfxMwADwBvgdPwDhQWYp5FbAI5Eu8vADMAbVhzHuAhxLGLrLA1JfmLcBNGcYuboEO37wDuI/AfAjzHPUh5vXARQTGLs4BBt+8GziMwXwfqPLMG4GzGMxdnAKNnnkbcB2juQWWPfM6IB2jeTqIqQrYitHcAhOeeTWwGqO5BZY887IvWQALnnkFsByDsQWWPPMY3+AXMOuZx/UNfgIznnmcSTYUMI/yJxsOmEeV5GQR8yiSvFfEvNwk7xUxLzXJe0XMwyZ5r4R52CTvlTD3k7wXwjzTBmyUYJwCWsMk+d8D+AA+gDfgGdgDhoDKb2v+W/EO8QdHzVvgE/UAAAAASUVORK5CYII=',
+      //   task: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAA5ElEQVR4nO2UQQrCMBBFcwc9g3oE8SbqUhFX4lVE8CCC4ELduBc8guhSPIDoRqEmMGAkTZNJU3DhLNPkvzzm/0CVlBLQAGbABjgBV2APLIAu8BJ5DcwBPXAGbsAWGALVogKgBaQ5MQHqvxhXgWHO4SPQyTGuAOOChrP/GZt3n8/ZEL0Cc2VjAzjKmk+hCHgHekrGfeAuAStN4wh4ABMl477MRUArGiNgJGuekjHwlLmIjxHwkjVNxsCnFIAWGRcxzgOIL6UAtMjYFAFoFxkXMU4y/v8Bwiv6BYBvXgpAMi4FkGScFOADFzgXQIbf8AQAAAAASUVORK5CYII='
+      // };
 
       const docDefinition: TDocumentDefinitions = {
         content: [
           {
             columns: [
-              {
-                image: logoDataURL, // Use the base64 data URL here
-                width: 150,
-              },
+              // {
+              //   image: logoDataURL, // Use the base64 data URL here
+              //   width: 150,
+              // },
               {
                 text: 'INVOICE',
                 alignment: 'right',
@@ -479,25 +479,25 @@ export class ArchitectDashboardComponent implements OnInit {
                   { text: 'Invoice Details', style: 'subheader' },
                   {
                     columns: [
-                      { width: 20, image: icons.invoice, fit: [20, 20] },
+                      // { width: 20, image: icons.invoice, fit: [20, 20] },
                       { width: 'auto', text: `Invoice ID: ${invoice.id || 'N/A'}`, margin: [5, 2, 0, 0] }
                     ]
                   },
                   {
                     columns: [
-                      { width: 20, image: icons.calendar, fit: [20, 20] },
+                      // { width: 20, image: icons.calendar, fit: [20, 20] },
                       { width: 'auto', text: `Emission Date: ${invoice.emissionDate || 'N/A'}`, margin: [5, 2, 0, 0] }
                     ]
                   },
                   {
                     columns: [
-                      { width: 20, image: icons.user, fit: [20, 20] },
+                      // { width: 20, image: icons.user, fit: [20, 20] },
                       { width: 'auto', text: `User ID: ${invoice.userId || 'N/A'}`, margin: [5, 2, 0, 0] }
                     ]
                   },
                   {
                     columns: [
-                      { width: 20, image: icons.task, fit: [20, 20] },
+                      // { width: 20, image: icons.task, fit: [20, 20] },
                       { width: 'auto', text: `Task ID: ${invoice.taskId || 'N/A'}`, margin: [5, 2, 0, 0] }
                     ]
                   },
