6844a04d6422c99790cde47a72236eecc9a4dab7
kholoud001 <kholoud.sanak@gmail.com>
2025-03-18 09:58:25 +0000

    update CI/CD pipeline 1

--- constructi-backend/src/test/java/com/constructi/ConstructiApplicationTests.java
+++ constructi-backend/src/test/java/com/constructi/ConstructiApplicationTests.java
@@ -2,8 +2,10 @@ package com.constructi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = "spring.profiles.active=test")
+@SpringBootTest
+@ActiveProfiles("test")
 class ConstructiApplicationTests {
 
     @Test
