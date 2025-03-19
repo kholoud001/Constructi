733fafcb74c607e6f7a7075a1a799c3673bb816d
kholoud001 <kholoud.sanak@gmail.com>
2025-03-19 09:24:56 +0000

    .env back

--- constructi-backend/src/main/java/com/constructi/ConstructiApplication.java
+++ constructi-backend/src/main/java/com/constructi/ConstructiApplication.java
@@ -1,5 +1,6 @@
 package com.constructi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ConstructiApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+
         SpringApplication.run(ConstructiApplication.class, args);
     }
 
--- constructi-backend/src/test/java/com/constructi/ConstructiApplicationTests.java
+++ constructi-backend/src/test/java/com/constructi/ConstructiApplicationTests.java
@@ -8,6 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class ConstructiApplicationTests {
 
+
+
     @Test
     void contextLoads() {
     }
